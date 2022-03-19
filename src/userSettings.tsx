e600e12ffed1bf7a5cc2ba363c84b912ea7c6006
huzefa69 <huzefa236@gmail.com>
2022-03-19 15:18:46 +0500

    a

--- src/userSettings.tsx
+++ src/userSettings.tsx
@@ -100,7 +100,7 @@ export const whitelistSettings: WhitelistSettings = {
   enabled: true,
 
   startDate: date("1 Jan 2022 00:00:00 GMT"),
-  endDate: date("20 May 2022 00:00:00 GMT"),
+  endDate: date("30 April 2022 00:00:00 GMT"),
   countdown: true,
   //Example date below
   // date('29 2021 00:00:00 GMT')
@@ -138,7 +138,7 @@ export class MintWhitelistCustomHTML extends React.Component {
 export const publicSaleSettings: PublicSaleSettings = {
   //start date and end date must match your Candy Machine Config for public launch!
 
-  startDate: date("7 Jan 2022 00:00:00 GMT"),
+  startDate: date("01 May 2022 00:00:00 GMT"),
   endDate: undefined,
   countdown: false,
   //Example date below
